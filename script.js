3ded7bd4897b80646ce09d1884c04593b57f37f9
vijaykumarorsu <162823807+vijaykumarorsu@users.noreply.github.com>
2024-09-23 00:53:08 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+
+window.addEventListener('scroll', () => {
+    const navbar = document.querySelector('.navbar');
+    if (window.scrollY > 100) {
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+    }
+});
